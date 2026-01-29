47a1d3b3c5c7a4232bd9918384f5a300c4752a0c
usagi0920 <mayu.pochi0920@gmail.com>
2026-01-30 08:22:21 +0900

    first commit

---
+++ src/views/CounterView/index.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { useState } from "react"
+
+export function CounterView() {
+    const [count, setCount] = useState(0)
+    const handlePlusButtonClick = () =>{
+        setCount(count + 1)
+    }
+
+
+  return (
+    <div style={{ maxWidth: 400, margin: "40px auto", textAlign: "center" }}>
+      <h1>カウンター</h1>
+      <p style={{ fontSize: 32 }}>{count}</p>
+      <div style={{ marginTop: 16 }}>
+        <button onClick={handlePlusButtonClick}>+1</button>
+      </div>
+    </div>
+  )
+}
